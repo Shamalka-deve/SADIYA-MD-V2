f8ae4b01917e65914c6df61a3ba7853b44d98bb0
Shamalka-deve <hmshamalkasubasinghe@gmail.com>
2025-05-09 07:52:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,9 +4,9 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //Put Session-id Here
-GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "", //Put Your Gitub Username
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "", //Put Your Github Auth Token
+SESSION_ID: process.env.SESSION_ID || "SADIYA-MD=2IdQUTaI#qTztk0a-Rvz8-G9DukUZyTDc0LI2Di3Jm1UgD16u5yY", //Put Session-id Here
+GITHUB_USER_NAME: process.env.GITHUB_USER_NAME || "Shamalka-deve", //Put Your Gitub Username
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "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", //Put Your Github Auth Token
 DATABASE_REPO_NAME: process.env.DATABASE_REPO_NAME || "SADIYA-MD-DATABASE", //Bot Database Repo Name
 SESSION_NAME: process.env.SESSION_NAME || "session"
 };
